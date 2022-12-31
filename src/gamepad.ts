7031763a5d136eb17ee5d65ee3e18d0dee73b9ae
Julien Graziano <julien@graziano.fr>
2022-12-31 22:25:35 +0100

    refactor: display gamepad when connected

--- src/gamepad.ts
+++ src/gamepad.ts
@@ -1,36 +1,47 @@
+import Dualsense from "./assets/dualsense.svg?raw"
+
+const GamePadFigure = {
+    "DualSense Wireless Controller Extended Gamepad": Dualsense
+}
+
+function createGamePadCard(gamepad: Gamepad): HTMLDivElement {
+    const card = document.createElement("div")
+
+    card.classList.add("card")
+    if (gamepad.id in GamePadFigure) {
+        const figure = document.createElement("figure")
+        const caption = document.createElement("figcaption")
+
+        caption.innerHTML = gamepad.id
+        figure.innerHTML = (GamePadFigure as any)[gamepad.id]
+        figure.appendChild(caption)
+
+        card.appendChild(figure)
+    }
+    return card
+}
+
 function createGamepadHandler(el: HTMLDivElement) {
-    const gamepads = {}
+    const gamepads = new Map<number, Gamepad>()
 
     return (event: GamepadEvent, connecting: boolean) => {
         const gamepad = event.gamepad
+
         if (connecting) {
-            console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-                gamepad.index,
-                gamepad.id,
-                gamepad.buttons.length,
-                gamepad.axes.length
-            )
-            ;(gamepads as any)[gamepad.index] = gamepad
+            gamepads.set(gamepad.index, gamepad)
         } else {
-            console.log("Gamepad disconnected from index %d: %s",
-                gamepad.index,
-                gamepad.id
-            )
-            delete (gamepads as any)[gamepad.index]
+            gamepads.delete(gamepad.index)
         }
 
         const gamepadList = document.createElement("ul")
-        for (const [index, gamepad] of Object.entries(gamepads) as Array<[string, Gamepad]>) {
+        gamepadList.classList.add("gamepad-list")
+
+        for (const [, gamepad] of gamepads) {
             const gamepadItem = document.createElement("li")
-            gamepadItem.innerHTML = `
-            <div class="card">
-                <h2>${Number(index) + 1}: ${gamepad.id}</h2>
-                <div class="card-content">
-                </div>
-            </div>
-            `
+            gamepadItem.appendChild(createGamePadCard(gamepad))
             gamepadList.appendChild(gamepadItem)
         }
+
         el.innerHTML = ""
         el.appendChild(gamepadList)
     }
